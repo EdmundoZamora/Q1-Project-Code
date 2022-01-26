71715c64433a35bb8232f64b3bfee48c56739632
mugen13le <mugen4college@gmail.com>
2022-01-25 20:51:03 -0800

    adding files for experiment 1 of tweetynet

---
+++ src/experiment1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo Bird0
+python3 experiment_1.py Bird0 600 > bird0_600.out
+echo Bird1
+python3 experiment_1.py Bird1 600 > bird1_600.out
+echo Bird2
+python3 experiment_1.py Bird2 600 > bird2_600.out
+echo Bird3
+python3 experiment_1.py Bird3 600 > bird3_600.out
+echo Bird4
+python3 experiment_1.py Bird4 600 > bird4_600.out
+echo Done
+#echo Bird5
+#python3 experiment_1.py Bird5 300 > bird5_300.out
+#echo Bird6
+#python3 experiment_1.py Bird6 300 > bird6_300.out
+#echo Bird7
+#python3 experiment_1.py Bird7 300 > bird7_300.out
+#echo Bird8
+#python3 experiment_1.py Bird8 300 > bird8_300.out
+#echo Bird9
+#python3 experiment_1.py Bird9 300 > bird9_300.out
+#echo Bird10
+#python3 experiment_1.py Bird10 300 > bird10_300.out
+#echo Done
